678149db52ec4938101bbdc61dd0cb536a7700df
derrick <derrickmbarani@gmail.com>
2023-02-27 19:27:57 +0300

    initial commit

---
+++ app/src/main/java/ke/derrick/steps/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package ke.derrick.steps.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Purple40 = Color(0xFF6650a4)
+val Pink40 = Color(0xFF7D5260)
+val Pink700 = Color(0xFFFF5666)
+
+val Blue20 = Color(0xFFEBF2FF)
+val Blue40 = Color(0xFFA4D2EB)
+val Blue200 = Color(0xFF56A7CB)
+val Blue400 = Color(0xFF5F8BDC)
+val Blue800 = Color(0xFF4184FD)
+
+val Green80 = Color(0xFFB3CB6C)
+val Green400 = Color(0xFF8BD26C)
